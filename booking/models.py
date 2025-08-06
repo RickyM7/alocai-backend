40cedfbd89c2dac917317f622877c747510d85d9
ynjisng <abreusara012@gmail.com>
2025-08-05 22:45:00 -0300

    feat: add models e migrations

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from login.models import Usuario
+from resource.models import Recurso
+
 # Create your models here.
+class Agendamento(models.Model):
+    id_agendamento = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column='id_usuario')
+    id_recurso = models.ForeignKey(Recurso, on_delete=models.CASCADE, db_column='id_recurso')
+    data_inicio = models.DateField()
+    hora_inicio = models.TimeField()
+    data_fim = models.DateField()
+    hora_fim = models.TimeField()
+    status_agendamento = models.CharField(max_length=20)
+    finalidade = models.CharField(max_length=255, null=True, blank=True)
+    observacoes = models.TextField(null=True, blank=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_ultima_atualizacao = models.DateTimeField(auto_now=True)
+    id_responsavel = models.IntegerField()
+
+    class Meta:
+        db_table = 'agendamento'
--- resource/models.py
+++ resource/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Recurso(models.Model):
+    id_recurso = models.AutoField(primary_key=True)
+    #id_instituicao = models.IntegerField()
+    nome_recurso = models.CharField(max_length=255)
+    #id_tipo_recurso = models.IntegerField()
+    descricao = models.TextField(null=True, blank=True)
+    capacidade = models.IntegerField(null=True, blank=True)
+    localizacao = models.CharField(max_length=255, null=True, blank=True)
+    status_recurso = models.CharField(max_length=20)
+    politicas_uso_especificas = models.TextField(null=True, blank=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'recurso'
